9f1c5c83b515923da890d7b66a3dc93aa154dbaf
Navi91 <navi91@inbox.ru>
2017-10-20 20:30:53 +0300

    Added map creator.

--- app/src/main/java/com/doubletapp/hermitage/hermitage/model/map/Room.java
+++ app/src/main/java/com/doubletapp/hermitage/hermitage/model/map/Room.java
@@ -1,5 +1,6 @@
 package com.doubletapp.hermitage.hermitage.model.map;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,11 +11,52 @@ public class Room {
     private Position mPosition;
     private List<Pass> mPasses;
 
+    public Room(int x, int y, Pass... passes) {
+        mPosition = new Position(x, y);
+        mPasses = Arrays.asList(passes);
+
+        for (Pass pass : passes) {
+            pass.addRoom(this);
+        }
+    }
+
     public Position getPosition() {
         return mPosition;
     }
 
     public List<Pass> getPasses() {
         return mPasses;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Room)) {
+            return false;
+        }
+
+        Room room = (Room) obj;
+
+        return mPosition.equals(room.getPosition());
+    }
+
+    public static Builder build(int x, int y) {
+        return new Builder(x, y);
+    }
+
+    public static class Builder {
+        private Room room;
+
+        private Builder(int x, int y) {
+            room = new Room(x, y);
+        }
+
+        public Builder pass(int x, int y) {
+            room.mPasses.add(new Pass(x, y));
+            return this;
+        }
+
+        public Room create() {
+            return room;
+        }
     }
 }
