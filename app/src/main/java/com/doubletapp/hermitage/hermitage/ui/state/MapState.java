f54884034aebb163d4484cc9da55fac7b083f856
Navi91 <navi91@inbox.ru>
2017-10-20 20:47:59 +0300

    Added rxjava,

---
+++ app/src/main/java/com/doubletapp/hermitage/hermitage/ui/state/MapState.java
@@ -0,0 +1,11 @@
+package com.doubletapp.hermitage.hermitage.ui.state;
+
+import java.util.List;
+
+/**
+ * Created by navi9 on 20.10.2017.
+ */
+
+public class MapState {
+    private List<RoomState> mRoomStates;
+}
