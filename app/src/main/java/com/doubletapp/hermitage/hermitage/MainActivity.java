e2678cccb8cc29830c611fd40f6292d71b1cabc3
gangsta <gangsta@doubletapp.ru>
2017-10-21 05:36:24 +0300

    Dijkstra's algorithm for pathes

--- app/src/main/java/com/doubletapp/hermitage/hermitage/MainActivity.java
+++ app/src/main/java/com/doubletapp/hermitage/hermitage/MainActivity.java
@@ -1,7 +1,7 @@
 package com.doubletapp.hermitage.hermitage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.doubletapp.hermitage.hermitage.ui.TabActivity;
 
--- app/src/main/java/com/doubletapp/hermitage/hermitage/model/PathBuilder.java
+++ app/src/main/java/com/doubletapp/hermitage/hermitage/model/PathBuilder.java
@@ -1,123 +1,193 @@
 package com.doubletapp.hermitage.hermitage.model;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import com.doubletapp.hermitage.hermitage.model.map.Pass;
 import com.doubletapp.hermitage.hermitage.model.map.Path;
 import com.doubletapp.hermitage.hermitage.model.map.Position;
+import com.doubletapp.hermitage.hermitage.model.map.Room;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class PathBuilder {
 
-    private Hall fromHall;
-    private Hall toHall;
-    private Position fromPosition;
-    private List<Hall> allHalls;
+    private List<Room> allRooms;
+    private Map<Pass, List<Pass>> pathRelations;
 
-    PathBuilder(Hall fromHall, Hall toHall, @Nullable Position fromPosition, List<Hall> allHalls) {
-        this.fromHall = fromHall;
-        this.toHall = toHall;
-        this.allHalls = allHalls;
-        if (fromPosition != null) {
-            this.fromPosition = fromPosition;
-        } else {
-            this.fromPosition = fromHall.getRoom().getPosition();
+    public PathBuilder(List<Room> allRooms) {
+        this.allRooms = allRooms;
+
+        initializePathRelations();
+    }
+
+    public PathBuilder(Room... rooms) {
+        this.allRooms = Arrays.asList(rooms);
+
+        initializePathRelations();
+    }
+
+    private void initializePathRelations() {
+        pathRelations = new HashMap<>();
+
+        for (Room room: allRooms) {
+            List<Pass> passes = room.getPasses();
+
+            for (int i = 0; i < passes.size(); i++) {
+                for (int j = 0; j < passes.size(); j++) {
+                    if (i != j) {
+                        addPassRelation(pathRelations, passes.get(i), passes.get(j));
+                    }
+                }
+            }
         }
     }
 
-    public @NonNull Path getPath() {
-        Map<Pass, List<PassPair>> pathPairsForPasses = getPathPairsForPasses();
-
-        Pass[] allPasses = (Pass[])pathPairsForPasses.keySet().toArray();
-
-
-        return new Path();
-    }
-
-    private Map<Pass, List<PassPair>> getPathPairsForPasses() {
-        Map<Pass, List<PassPair>> pathPairsForPasses = new HashMap<>();
-
-        List<PassPair> allPasses = getPathPairs();
-
-        for(PassPair pair: allPasses) {
-            addPairToPass(pathPairsForPasses, pair, pair.firstPass);
-            addPairToPass(pathPairsForPasses, pair, pair.secondPass);
+    private void addPassRelation(Map<Pass, List<Pass>> roomRelations, Pass passKey, Pass passRelation) {
+        if (!roomRelations.containsKey(passKey)) {
+            roomRelations.put(passKey, new ArrayList<Pass>());
         }
 
-        return pathPairsForPasses;
+        roomRelations.get(passKey).add(passRelation);
     }
 
-    private void addPairToPass(Map<Pass, List<PassPair>> pathPairsForPasses, PassPair pair, Pass pass) {
-        if (!pathPairsForPasses.containsKey(pass)) {
-            pathPairsForPasses.put(pass, new ArrayList<PassPair>());
+    public @NonNull Path getPath(Hall fromHall, List<Hall> halls, Position fromPosition) {
+
+        double minDistance = Double.MAX_VALUE;
+        List<Pass> minDistancePlasses = null;
+
+        for (Pass pass: fromHall.getRoom().getPasses()) {
+            List<Pass> listOfPasses = pathFromPass(pass, halls);
+            double distance = 0;
+            for(int i = 0; i < listOfPasses.size() - 1; i++) {
+                distance += listOfPasses.get(i).getDistanceTo(listOfPasses.get(i + 1));
+            }
+            if (distance < minDistance) {
+                minDistance = distance;
+                minDistancePlasses = listOfPasses;
+            }
         }
 
-        pathPairsForPasses.get(pass).add(pair);
+        Path path = new Path();
+        path.addPasses(minDistancePlasses);
+
+        return path;
     }
 
-    private List<PassPair> getPathPairs() {
-        ArrayList<PassPair> allPasses = new ArrayList<>();
-        for(Hall hall: allHalls) {
-            List<Pass> passes = hall.getRoom().getPasses();
-            for(int i = 0; i < passes.size() - 2; i ++) {
-                for(int j = i + 1; j < passes.size() - 1; j++) {
-                    allPasses.add(new PassPair(passes.get(i), passes.get(j)));
+    private List<Pass> pathFromPass(Pass rootPass, List<Hall> halls) {
+        Set<Pass> keyset = pathRelations.keySet();
+        Pass[] passes = keyset.toArray(new Pass[keyset.size()]);
+        int[] previousPasses = getPreviouses(rootPass, passes);
+
+        List<Pass> listOfPasses = Arrays.asList(passes);
+
+        Pass minPass = null;
+        double minDistance = Double.MAX_VALUE;
+
+        for (Hall hall: halls) {
+            for (Pass pass: hall.getRoom().getPasses()) {
+
+                double distance = getDistance(pass, listOfPasses, previousPasses);
+
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    minPass = pass;
                 }
             }
         }
 
-        return allPasses;
+        List<Pass> path = new LinkedList<>();
+
+        int indexOfPass = listOfPasses.indexOf(minPass);
+
+        while (previousPasses[indexOfPass] != -1) {
+            path.add(0, passes[indexOfPass]);
+            indexOfPass = previousPasses[indexOfPass];
+        }
+
+        path.add(0, rootPass);
+
+        return path;
     }
 
+    private double getDistance(Pass fromPass, List<Pass> listOfPasses, int[] previousPasses) {
+        int indexOfPass = listOfPasses.indexOf(fromPass);
 
+        double distance = 0.0;
 
-
-
-
-
-
-    static class PassPair {
-        private Pass firstPass;
-        private Pass secondPass;
-
-        PassPair(Pass firstPass, Pass secondPass) {
-            this.firstPass = firstPass;
-            this.secondPass = secondPass;
+        while (previousPasses[indexOfPass] != -1) {
+            distance += listOfPasses.get(indexOfPass).getDistanceTo(listOfPasses.get(previousPasses[indexOfPass]));
+            indexOfPass = previousPasses[indexOfPass];
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof PassPair) {
-                if (firstPass.equals(((PassPair) obj).getFirstPass()) && secondPass.equals(((PassPair) obj).getSecondPass())) {
-                    return true;
-                }
-                if (firstPass.equals(((PassPair) obj).getSecondPass()) && secondPass.equals(((PassPair) obj).getFirstPass())) {
-                    return true;
+        return distance;
+    }
+
+    private int[] getPreviouses(Pass rootPass, Pass[] passes) {
+
+        int capacity = passes.length;
+
+        double[][] connections = new double[capacity][capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            Pass pass = passes[i];
+            List<Pass> connectedPasses = pathRelations.get(pass);
+            for (int j = 0; j < capacity; j++) {
+                if(connectedPasses.contains(passes[j])) {
+                    connections[i][j] = pass.getDistanceTo(passes[j]);
+                } else {
+                    connections[i][j] = Double.MAX_VALUE;
                 }
             }
-            return false;
         }
 
-        @Override
-        public int hashCode() {
-            return firstPass.hashCode() + secondPass.hashCode();
-        }
+        double[] distances = new double[capacity];
+        int[] previous = new int[capacity];
+        boolean[] used = new boolean[capacity];
 
-        Pass getFirstPass() {
-            return firstPass;
-        }
+        int indexOfRootPath = Arrays.asList(passes).indexOf(rootPass);
 
-        Pass getSecondPass() {
-            return secondPass;
-        }
+        Arrays.fill(distances, Double.MAX_VALUE);
+        Arrays.fill(previous, -1);
+        Arrays.fill(used, false);
 
-        double getDistance() {
-            return firstPass.getDistanceTo(secondPass);
-        }
+        distances[indexOfRootPath] = 0;
+        previous[indexOfRootPath] = -1;
+
+        int minIndex;
+        double temp;
+
+        do {
+            minIndex = -1;
+            double min = Double.MAX_VALUE;
+
+            for (int i = 0; i < capacity; i++) {
+                if (!used[i] && distances[i] < min) {
+                    min = distances[i];
+                    minIndex = i;
+                }
+            }
+
+            if (minIndex != -1) {
+                for (int i = 0; i < capacity; i++) {
+                    if (connections[minIndex][i] < Double.MAX_VALUE) {
+                        temp = min + connections[minIndex][i];
+                        if (temp < distances[i]) {
+                            distances[i] = temp;
+                            previous[i] = minIndex;
+                        }
+                    }
+                }
+
+                used[minIndex] = true;
+            }
+        } while (minIndex != -1);
+
+        return previous;
     }
 }
