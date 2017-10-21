d5c93cd33541eef88fc1a130907d9ef46f9c9a44
Dmitriy <voronov@xaker.ru>
2017-10-21 04:48:22 +0300

    Merge remote-tracking branch 'remotes/origin/1_login_screen' into dev
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/doubletapp/hermitage/hermitage/ui/ticket/BuyTicketActivity.java
+++ app/src/main/java/com/doubletapp/hermitage/hermitage/ui/ticket/BuyTicketActivity.java
@@ -2,14 +2,22 @@ package com.doubletapp.hermitage.hermitage.ui.ticket;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 import com.doubletapp.hermitage.hermitage.R;
 import com.doubletapp.hermitage.hermitage.utils.ActivityUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class BuyTicketActivity extends AppCompatActivity {
+
+    @BindView(R.id.buy_ticket_toolbar)
+    Toolbar mToolbar;
 
     public static void start(@NonNull Context context) {
         Intent starter = new Intent(context, BuyTicketActivity.class);
@@ -20,6 +28,13 @@ public class BuyTicketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_ticket);
+        ButterKnife.bind(this);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
         ActivityUtils.replaceFragment(getSupportFragmentManager(),
                 R.id.fragment_container,
                 BuyTicketFragment.newInstance(),
